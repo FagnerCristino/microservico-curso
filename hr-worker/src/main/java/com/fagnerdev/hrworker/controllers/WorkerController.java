60e80177d4fdcc4e38b6ccdfc20a00b8f3fd3eff
Fagner Cristino <fcristino.sc48@gmail.com>
2021-02-26 09:46:35 -0300

    Hystrix fault tolerance

--- hr-worker/src/main/java/com/fagnerdev/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/fagnerdev/hrworker/controllers/WorkerController.java
@@ -37,6 +37,13 @@ public class WorkerController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+//    	try {
+//			Thread.sleep(3000L);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
     	
     	logger.info("PORT = " + environment.getProperty("local.server.port"));
         Worker obj = workerRepository.findById(id).get();
